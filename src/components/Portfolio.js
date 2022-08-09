f5c23180c73f12ba943f5f793d3d514df7db98ab
Althea Hanson <altheacodes@gmail.com>
2022-08-09 19:23:22 -0400

    changed portfolio/project display

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,25 +1,45 @@
 import React from 'react';
 import './styles/project.css'
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LaunchIcon from '@mui/icons-material/Launch';
+
 
 const Portfolio = (props) => {
   return (
-    
-    <div className="card m-3 ">
-
-    <h5 className="card-header">{props.title}</h5>
-
-    <div className="card-body">
-      <img src={props.image} className="card-img-top" alt={props.alt} />
-    </div>
-
-    <div className=" mx-auto mb-4">
-      <a href={props.repository} rel="noreferrer" target="_blank" className="btn btn-primary">Repository</a>
-      <a href={props.deployed} rel="noreferrer" target="_blank" className="btn btn-primary">Deployed</a>
-    </div>
-    
-  </div>
   
+<Card className="card" sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt={props.alt}
+        height="140"
+        image={props.image}
+      />
+      <CardContent>
+        <Typography className="card-header" gutterBottom variant="h5" component="div">
+        {props.title}
+        </Typography>
+        <Typography className="card-body" variant="body2" color="text.secondary">
+        {props.description}
+        </Typography>
+      </CardContent>
+      <CardActions className="card-links">
+      <a href={props.repository} rel="noreferrer" target="_blank" className="btn btn-primary">
+        <GitHubIcon sx={{ fontSize: "30px" }} /></a>
+
+        <a href={props.deployed} rel="noreferrer" target="_blank" className="btn btn-primary">
+          <LaunchIcon sx={{ fontSize: "30px" }} /></a>
+  
+      </CardActions>
+    </Card>
+
+
+
   )
 }
 
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -10,6 +10,7 @@ const projects = [
     title: "Movie Vs Trivia",
     image: Images.MovieVsTriviaLogo,
     alt: "",
+    description: "This is a full stack collaborative project demonstrates the use of models, views and controllers.",
     repository: "https://github.com/DlonMusk/fullstack-movie-vs-trivia",
     deployed: "https://serene-carlsbad-caverns-28814.herokuapp.com/login"
   },
@@ -17,6 +18,7 @@ const projects = [
     title: " Explosive TicTacToe",
      image:Images.TicTacToe ,
     alt: "",
+    description: "A real time application that allows users to play tic-tac-toe and chat with a friend online. This application uses the MERN stack and features the use of Socket.IO to create real-time connections between players.",
     repository: "https://github.com/RichardZhang01/explosive-tic-tac-toe",
     deployed: "https://explosive-tic-tac-toe.herokuapp.com/"
   },
@@ -24,6 +26,7 @@ const projects = [
     title: "Rewind",
    image: Images.ReWindLogo,
     alt: "",
+    description: "This project demonstrates the use of Javascript loops to reverse a string.",
     repository: "https://github.com/Altheahanson/Rewind",
     deployed: "https://rewind-a-javascript-project.netlify.app"
   },
@@ -31,6 +34,7 @@ const projects = [
     title: "Tech Blog",
      image: Images.techblog ,
     alt: "",
+    description: "An ExpressJS SQL Tech blog where users may sign in to add posts and comments",
     repository: "https://github.com/Altheahanson/TechBlog",
     deployed: "https://pacific-atoll-54698.herokuapp.com/"
   },
@@ -38,6 +42,7 @@ const projects = [
     title: "Weather Dashboard",
      image: Images.weatherDashboardLogo ,
     alt: "",
+    description: "A weather dashboard that allows the user to get a city's current weather conditions and the five day forecast. Third-party APIs are used to access weather data by making requests with specific parameters to a URL.",
     repository: "https://github.com/Altheahanson/weatherDashboard",
     deployed: "https://altheahanson.github.io/weatherDashboard/"
   },
@@ -45,6 +50,7 @@ const projects = [
     title: "workdayscheduler",
      image: Images.WorkDayScheduler ,
     alt: "",
+    description: "A simple calendar app for scheduling appointments and planning out your work day. This project utilizes JQuery, JavaScript and Bootstrap.",
     repository: "https://github.com/Altheahanson/workdayscheduler",
     deployed: "https://altheahanson.github.io/workdayscheduler/"
   },
@@ -59,7 +65,7 @@ const Projects = () => {
       <h2>Projects</h2>
       <div className="work-container"  >
         {projects.map( (project) =>
-          <Portfolio title={project.title} image={project.image} alt={project.alt} repository={project.repository} deployed={project.deployed}/>
+          <Portfolio title={project.title} image={project.image} alt={project.alt} repository={project.repository} deployed={project.deployed} description={project.description}/>
         )}
       </div>
     </section>
